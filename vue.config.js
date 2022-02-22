853154f6428b9bd9737680183ffcaebcdad6c6c3
mercutio <gycabc123@qq.com>
2022-02-22 20:03:35 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,6 @@ module.exports = {
     },
     lintOnSave: false,
     // outputDir: 'dist',
-    publicPath: process.env.NODE_ENV === 'production' ? '/lightie-nav/' : '/',
-    // publicPath: './'
+    // publicPath: process.env.NODE_ENV === 'production' ? '/lightie-nav/' : '/',
+    publicPath: './'
 }
